67022a53705e4c5c6aa9e0107340033c4402ea06
Rusiru <rusiru091@gmail.com>
2017-05-28 19:10:16 +0530

    added hibernate

---
+++ src/main/java/com/leaf/cashier/Utility/CommonConstant.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leaf.cashier.Utility;
+
+/**
+ *
+ * @author Rusiru
+ */
+public class CommonConstant {
+    
+    private static final String RESOURCES_FILE_PATH = "/resources";
+    public static final String HIBERNATE_PROPERTIES_FILE_PATH = RESOURCES_FILE_PATH + "/hibernate.properties";
+    public static final String HIBERNATE_CONFIGURATION_FILE_PATH = RESOURCES_FILE_PATH + "/hibernate.cfg.xml";
+}
---
+++ src/main/java/com/leaf/cashier/Utility/HaibernateUtil.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leaf.cashier.Utility;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+/**
+ *
+ * @author Rusiru
+ */
+public class HaibernateUtil {
+
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+
+                Map<String, String> settings = new HashMap<>();
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, "jdbc:postgresql://127.0.0.1:5432/inventory");
+                settings.put(Environment.USER, "postgres");
+                settings.put(Environment.PASS, "0344921685");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+                settings.put(Environment.POOL_SIZE, "1");
+                settings.put(Environment.FORMAT_SQL, "true");
+                settings.put(Environment.SHOW_SQL, "true");
+
+                registryBuilder.applySettings(settings);
+                registry = registryBuilder.build();
+                MetadataSources sources = new MetadataSources(registry);
+                Metadata metadata = sources.getMetadataBuilder().build();
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
